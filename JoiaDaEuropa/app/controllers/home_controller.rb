17cc7b2acf88553df1d7c4f28c1817a09a05dae5
anazevedo <ana.azevedo98@gmail.com>
2017-05-05 10:24:52 +0100

    Modificação da página home para public.

--- JoiaDaEuropa/app/controllers/home_controller.rb
+++ JoiaDaEuropa/app/controllers/home_controller.rb
@@ -1,10 +1,17 @@
 class HomeController < ApplicationController
 
+  skip_before_action :authenticateuser!
+
+  ## ao fazer o skip, a página fica pública. Neste file criamos as
+  ## restantes páginas publicas (e.g.contacts, about us)
+  ## depois, ir ao routes.rb e adicionar as rotas: eg: get 'home/contacts'
   def index
-
-
 
   end
 
+ ##def contacts
+  ##end
 
+  ##def about_us
+  ##end
 end
