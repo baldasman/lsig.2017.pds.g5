5e34b461aead0909ef9243df5c50214bd8b4d529
Magolizen34 <luismagolo3@gmail.com>
2017-05-12 17:27:55 +0100

    Generate client_area

---
+++ JoiaDaEuropa/app/controllers/client_area_controller.rb
@@ -0,0 +1,13 @@
+class ClientAreaController < ApplicationController
+  def index
+    @account = current_user
+  end
+
+  def new_order
+
+  end
+
+  def save_order
+    @order = order
+  end
+end
--- JoiaDaEuropa/config/routes.rb
+++ JoiaDaEuropa/config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'client_area/index'
+
+  get 'client_area/new_order'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/profile/index'
-
-  root to: 'home#index'
+  get '/home/index'
+  get 'client_area/save_order'
+  post 'client_area/save_order'
+  root to: 'client_area#index'
 end
