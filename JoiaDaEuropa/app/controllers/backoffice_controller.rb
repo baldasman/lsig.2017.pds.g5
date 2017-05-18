d9048222b14cc5f5f8f3b39146728e6aab15ec51
Magolizen34 <luismagolo3@gmail.com>
2017-05-18 10:19:16 +0100

    backoffice_controller acrescentei admin

--- JoiaDaEuropa/app/controllers/backoffice_controller.rb
+++ JoiaDaEuropa/app/controllers/backoffice_controller.rb
@@ -1,6 +1,13 @@
 class BackofficeController < ApplicationController
 
-  before_action :authenticate_user!
+  before_filter :authorized?
+  private
+  def authorized?
+    unless current_user.has_role? :admin
+      flash[:error] = "You are not authorized to view that page."
+      redirect_to root_path
+    end
+  end
 
   def index
 
